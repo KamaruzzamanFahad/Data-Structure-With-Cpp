a667fcc201b9e6d922f92e44d5f20596040c4e2d
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2025-01-10 08:55:00 +0600

    dsa stack and que implement using singly and dubly link list exap questons solve

--- DSA_stack_queue_assignment_module-16/Same_or_Not_II.cpp
+++ DSA_stack_queue_assignment_module-16/Same_or_Not_II.cpp
@@ -1,85 +1,176 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-
-class Node{
-    public:
-
+class Node
+{
+public:
     int val;
-    Node* next;
-    Node* prev;
+    Node *next;
+    Node *prev;
 
-    Node(int val){
+    Node(int val)
+    {
         this->val = val;
         this->next = NULL;
         this->prev = NULL;
     }
 };
 
+class st
+{
 
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int siz = 0;
 
-class st{
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        siz++;
 
-    public:
-    Node* head = NULL;
-    Node* tail = NULL;
-
-    void push(int val){
-        Node* newNode = new Node(val)
-        
-    }
-    void pop(){
-
-    }
-
-    int size(){
-
-    }
-
-    bool empty(){
-
-    }
-
-    int top(){
-
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
     }
 
+    void pop()
+    {
+        siz--;
+        if (head != NULL)
+        {
+            Node *deleteNode = tail;
+            tail = tail->prev;
+            delete deleteNode;
+            if (tail == NULL)
+            {
+                head = NULL;
+                tail = NULL;
+                return;
+            }
+            tail->next = NULL;
+        }
+    }
+
+    int size()
+    {
+        return siz;
+    }
+
+    bool empty()
+    {
+        return head == NULL;
+    }
+
+    int top()
+    {
+        return tail->val;
+    }
 };
 
 
-int main(){
-    stack<int> st;
-    queue<int> qu;
 
-    int n,m;
+class que
+{
+
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int siz = 0;
+
+    void push(int val)
+    {
+        Node *newNode = new Node(val);
+        siz++;
+
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    void pop()
+    {
+        siz--;
+        if (head != NULL)
+        {
+            Node *deleteNode = head;
+            head = head->next;
+            delete deleteNode;
+            if (head == NULL)
+            {
+                tail = NULL;
+                return;
+            }
+           
+        }
+    }
+
+    int size()
+    {
+        return siz;
+    }
+
+    bool empty()
+    {
+        return head == NULL;
+    }
+
+    int front()
+    {
+        return head->val;
+    }
+};
+
+int main()
+{
+    st st;
+    que qu;
+
+    int n, m;
     cin >> n >> m;
-    if(n != m){
+    if (n != m)
+    {
         cout << "NO";
         return 0;
     }
 
-    for(int i=0; i<n; i++){
+    for (int i = 0; i < n; i++)
+    {
         int val;
         cin >> val;
         st.push(val);
     }
 
-     for(int i=0; i<m; i++){
+    for (int i = 0; i < m; i++)
+    {
         int val;
         cin >> val;
         qu.push(val);
     }
 
     bool flag = true;
-    while(!st.empty()){
-        if(st.top() != qu.front()){
+    while (!st.empty())
+    {
+        if (st.top() != qu.front())
+        {
             flag = false;
             break;
         }
         st.pop();
         qu.pop();
     }
-    
+
     flag ? cout << "YES" : cout << "NO";
     return 0;
 }
