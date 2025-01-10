47046b94411fcd2e973a79bb57b58c2f4a4f15f7
Kamaruzzaman Fahad <58844727+KamaruzzamanFahad@users.noreply.github.com>
2025-01-10 09:16:28 +0600

    dsa queue exam problem solve

---
+++ DSA_stack_queue_assignment_module-16/Is_It_Valid.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int q;
+    cin >> q;
+    cin.ignore();
+
+    for (int i = 0; i < q; i++)
+    {
+        string s;
+        getline(cin, s);
+
+        stack<char> st;
+        for (char c : s)
+        {
+            if (!st.empty() && ((c == '1' && st.top() == '0') || (c == '0' && st.top() == '1')))
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(c);
+            }
+        }
+        if (st.empty())
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+}
